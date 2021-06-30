ad6c1b64afcbf019fe040b918a3243552aa6a14b
Anjali Tyagi <anjalityagi988@gmail.com>
2021-06-30 15:38:56 -0400

    started going integrated testing on feature 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-
 import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-import NumberOfEvents from "./NumberOfEvents"
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents } from "./api";
+import { extractLocations } from "./api";
 
 class App extends Component {
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
+  state = {
+    events: [],
+    locations: [],
+  };
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
-        <NumberOfEvents/>
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+
+        <EventList events={this.state.events} />
+        <NumberOfEvents />
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+import { mockData } from ".//mock-data";
+import axios from "axios";
 /**
  *
  * @param {*} events:
@@ -6,8 +8,33 @@
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
 };
+
+export const getEvents = async () => {
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get("YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT");
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
