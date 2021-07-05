2b6cdb0f1850b25b92e62a52ccdd032a4a6cf4bd
Anjali Tyagi <anjalityagi988@gmail.com>
2021-07-05 02:43:17 -0400

    correction in App.js

--- src/App.js
+++ src/App.js
@@ -1,83 +1,9 @@
-// import React, { Component } from "react";
-// import "./App.css";
-// import EventList from "./EventList";
-// import CitySearch from "./CitySearch";
-// import NumberOfEvents from "./NumberOfEvents";
-// import { getEvents } from "./api";
-// import { extractLocations } from "./api";
-// import "./nprogress.css";
-
-// class App extends Component {
-//   componentDidMount() {
-//     this.mounted = true;
-//     getEvents().then((events) => {
-//       if (this.mounted) {
-//         this.setState({ events, locations: extractLocations(events) });
-//       }
-//     });
-//   }
-
-//   componentWillUnmount() {
-//     this.mounted = false;
-//   }
-//   updateEvents = (location) => {
-//     getEvents().then((events) => {
-//       const locationEvents =
-//         location === "all"
-//           ? events
-//           : events.filter((event) => event.location === location);
-//       this.setState({
-//         events: locationEvents,
-//       });
-//     });
-//   };
-
-//   updateEventsCount = (eventCount) => {
-//     getEvents().then((events) => {
-//       const eventCountEvents = events.filter(
-//         (event) => event.eventCount === eventCount
-//       );
-//       this.setState({
-//         events: eventCountEvents,
-//       });
-//     });
-//   };
-
-//   state = {
-//     events: [],
-//     locations: [],
-//     eventsPerPage: [],
-//   };
-//   render() {
-//     return (
-//       <div className="App">
-//         <CitySearch
-//           locations={this.state.locations}
-//           updateEvents={this.updateEvents}
-//           numberOfEvents={this.state.numberOfEvents}
-//         />
-
-//         <NumberOfEvents
-//           eventsPerPage={this.state.eventsPerPage}
-//           updateEventsCount={this.updateEventsCount}
-//         />
-
-//         <EventList events={this.state.events} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
 import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import NumberOfEvents from "./NumberOfEvents";
 import CitySearch from "./CitySearch";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-
+import { getEvents, extractLocations, checkToken } from "./api";
 
 class App extends Component {
   state = {
@@ -149,7 +75,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
