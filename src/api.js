97d634a6e0ed49ab637e777db574122eeb7ac44e
Anjali Tyagi <anjalityagi988@gmail.com>
2021-07-07 14:03:32 -0400

    changes to api.js

--- src/api.js
+++ src/api.js
@@ -1,141 +1,6 @@
-import { mockData } from "./mock-data";
-import axios from "axios";
-import NProgress from "nprogress";
-
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-
-  return result;
-};
-
-const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
-  return locations;
-};
-
-const getEvents = async () => {
-  NProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
-
-  if (!navigator.onLine) {
-    const events = localStorage.getItem("lastEvents");
-    NProgress.done();
-    console.log(events);
-    return events ? JSON.parse(events).events : [];
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =
-      "https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
-      "/" +
-      token;
-    const result = await axios.get(url);
-    if (result.data) {
-      var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-    }
-    NProgress.done();
-    return result.data.events;
-  }
-};
-
-const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch(
-    " https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
-      "/" +
-      encodeCode
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .catch((error) => error);
-
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
-export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
-
-
-
-// import axios from "axios";
 // import { mockData } from "./mock-data";
-
-// /* This function takes an events array, then uses map to create a new array with only locations. It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.The Set will remove all duplicates from the array. */
-
-// const checkToken = async (accessToken) => {
-//   const result = await fetch(
-//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-//   )
-//     .then((res) => res.json())
-//     .catch((error) => console.log(error));
-//   return result;
-// };
-
-// const getToken = async (code) => {
-//   const encodeCode = encodeURIComponent(code);
-//   const { access_token } = await fetch(
-//     `https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
-//   )
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .catch((error) => console.log(error));
-
-//   access_token && localStorage.setItem("access_token", access_token);
-
-//   return access_token;
-// };
+// import axios from "axios";
+// import NProgress from "nprogress";
 
 // const removeQuery = () => {
 //   if (window.history.pushState && window.location.pathname) {
@@ -151,39 +16,59 @@ export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
 //   }
 // };
 
-// export const extractLocations = (events) => {
-//   let extractLocations = events.map((event) => event.location);
-//   let locations = [...new Set(extractLocations)];
+// const checkToken = async (accessToken) => {
+//   const result = await fetch(
+//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+//   )
+//     .then((res) => res.json())
+//     .catch((error) => error.json());
+
+//   return result;
+// };
+
+// const extractLocations = (events) => {
+//   var extractLocations = events.map((event) => event.location);
+//   var locations = [...new Set(extractLocations)];
 //   return locations;
 // };
 
-// export const getEvents = async () => {
+// const getEvents = async () => {
+//   NProgress.start();
+
 //   if (window.location.href.startsWith("http://localhost")) {
+//     NProgress.done();
 //     return mockData;
 //   }
 
 //   if (!navigator.onLine) {
-//     const data = localStorage.getItem("lastEvents");
-//     return data ? JSON.parse(data).events : [];
+//     const events = localStorage.getItem("lastEvents");
+//     NProgress.done();
+//     console.log(events);
+//     return events ? JSON.parse(events).events : [];
 //   }
 
 //   const token = await getAccessToken();
+
 //   if (token) {
 //     removeQuery();
-//     const url = `https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
-
+//     const url =
+//       "https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
+//       "/" +
+//       token;
 //     const result = await axios.get(url);
 //     if (result.data) {
-//       let locations = extractLocations(result.data.events);
+//       var locations = extractLocations(result.data.events);
 //       localStorage.setItem("lastEvents", JSON.stringify(result.data));
 //       localStorage.setItem("locations", JSON.stringify(locations));
 //     }
+//     NProgress.done();
 //     return result.data.events;
 //   }
 // };
 
-// export const getAccessToken = async () => {
+// const getAccessToken = async () => {
 //   const accessToken = localStorage.getItem("access_token");
+
 //   const tokenCheck = accessToken && (await checkToken(accessToken));
 
 //   if (!accessToken || tokenCheck.error) {
@@ -201,3 +86,116 @@ export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
 //   }
 //   return accessToken;
 // };
+
+// const getToken = async (code) => {
+//   const encodeCode = encodeURIComponent(code);
+//   const { access_token } = await fetch(
+//     " https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
+//       "/" +
+//       encodeCode
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .catch((error) => error);
+
+//   access_token && localStorage.setItem("access_token", access_token);
+
+//   return access_token;
+// };
+
+// export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
+
+import axios from "axios";
+import { mockData } from "./mock-data";
+
+/* This function takes an events array, then uses map to create a new array with only locations. It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.The Set will remove all duplicates from the array. */
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+  return result;
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    `https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => console.log(error));
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+export const extractLocations = (events) => {
+  let extractLocations = events.map((event) => event.location);
+  let locations = [...new Set(extractLocations)];
+  return locations;
+};
+
+export const getEvents = async () => {
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    return data ? JSON.parse(data).events : [];
+  }
+
+  const token = await getAccessToken();
+  if (token) {
+    removeQuery();
+    const url = `https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
+
+    const result = await axios.get(url);
+    if (result.data) {
+      let locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    return result.data.events;
+  }
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://raro370h4k.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
