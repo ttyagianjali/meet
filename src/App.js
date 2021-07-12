613267203d920a7019cd811d3b878b4d840827f5
Anjali Tyagi <anjalityagi988@gmail.com>
2021-07-12 02:42:48 -0400

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,106 @@
+// import "./nprogress.css";
+// import React, { Component } from "react";
+// import "./App.css";
+// import EventList from "./EventList";
+// import NumberOfEvents from "./NumberOfEvents";
+// import CitySearch from "./CitySearch";
+// import { getEvents, extractLocations, checkToken } from "./api";
+
+// class App extends Component {
+//   state = {
+//     events: [],
+//     locations: [],
+//     numberOfEvents: 32,
+//     currentCity: "all",
+//   };
+
+//   updateEvents = (location, numberOfEvents) => {
+//     getEvents().then((events) => {
+//       const locationEvents =
+//         location === "all"
+//           ? events.slice(0, numberOfEvents)
+//           : events.filter((event) => event.location === location);
+//       if (this.mounted) {
+//         this.setState({
+//           events: locationEvents.slice(0, numberOfEvents),
+//           currentCity: location,
+//           //locations: [location],
+//         });
+//       }
+//     });
+//   };
+
+//   //In this function will update tbe number of events of app.state fom <NumberOfEvents>
+//   updateNumberOfEvents(eventNumber) {
+//     this.setState({ numberOfEvents: eventNumber });
+//     const { currentCity } = this.state;
+//     this.updateEvents(currentCity, eventNumber);
+//   }
+
+//   async componentDidMount() {
+//     const { numberOfEvents } = this.state;
+//     this.mounted = true;
+//     const accessToken = localStorage.getItem("access_token");
+//     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const code = searchParams.get("code");
+//     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+//     if ((code || isTokenValid) && this.mounted) {
+//       getEvents().then((events) => {
+//         if (this.mounted) {
+//           this.setState({
+//             events: events.slice(0, numberOfEvents),
+//             locations: extractLocations(events),
+//           });
+//         }
+//       });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.mounted = false;
+//   }
+
+//   // getData() {
+//   //   const { locations, events } = this.state;
+//   //   const data = locations.map((location) => {
+//   //     const number = events.filter(
+//   //       (event) => event.location === location
+//   //     ).length;
+//   //     const city = location.split(", ").shift(); //split the location at the occurrence of ", " and return an array- .shift() array function to get the first element in the array
+//   //     return { city, number };
+//   //   });
+//   //   return data;
+//   // }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <CitySearch
+//           locations={this.state.locations}
+//           updateEvents={this.updateEvents}
+//           numberOfEvents={this.state.numberOfEvents}
+//         />
+
+//         <NumberOfEvents
+//           updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)}
+//         />
+
+//         <EventList events={this.state.events} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import "./nprogress.css";
 import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import NumberOfEvents from "./NumberOfEvents";
 import CitySearch from "./CitySearch";
-import { getEvents, extractLocations, checkToken } from "./api";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
   state = {
@@ -24,13 +120,12 @@ class App extends Component {
         this.setState({
           events: locationEvents.slice(0, numberOfEvents),
           currentCity: location,
-          //locations: [location],
         });
       }
     });
   };
 
-  //In this function will update tbe number of events of app.state fom <NumberOfEvents>
+  //I this function will update tbe number of events of app.state fom <NumberOfEvents>
   updateNumberOfEvents(eventNumber) {
     this.setState({ numberOfEvents: eventNumber });
     const { currentCity } = this.state;
@@ -40,12 +135,10 @@ class App extends Component {
   async componentDidMount() {
     const { numberOfEvents } = this.state;
     this.mounted = true;
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+    this.setState({ showWelcomeScreen: !code });
+    if (code && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
           this.setState({
@@ -60,18 +153,6 @@ class App extends Component {
   componentWillUnmount() {
     this.mounted = false;
   }
-
-  // getData() {
-  //   const { locations, events } = this.state;
-  //   const data = locations.map((location) => {
-  //     const number = events.filter(
-  //       (event) => event.location === location
-  //     ).length;
-  //     const city = location.split(", ").shift(); //split the location at the occurrence of ", " and return an array- .shift() array function to get the first element in the array
-  //     return { city, number };
-  //   });
-  //   return data;
-  // }
 
   render() {
     return (
@@ -93,3 +174,4 @@ class App extends Component {
 }
 
 export default App;
+
