f13230f4793f52c9897954caa668857feabacc4d
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-18 00:27:33 -0400

    tests passed with 100% coverage

--- src/App.js
+++ src/App.js
@@ -18,3 +18,85 @@ class App extends Component {
 }
 
 export default App;
+
+// import "./nprogress.css";
+// import React, { Component } from "react";
+// import "./App.css";
+// import EventList from "./EventList";
+// import NumberOfEvents from "./NumberOfEvents";
+// import CitySearch from "./CitySearch";
+// import { getEvents, extractLocations } from "./api";
+
+// class App extends Component {
+//   state = {
+//     events: [],
+//     locations: [],
+//     numberOfEvents: 32,
+//     currentCity: "all",
+//   };
+
+//   updateEvents = (location, numberOfEvents) => {
+//     getEvents().then((events) => {
+//       const locationEvents =
+//         location === "all"
+//           ? events.slice(0, numberOfEvents)
+//           : events.filter((event) => event.location === location);
+//       if (this.mounted) {
+//         this.setState({
+//           events: locationEvents.slice(0, numberOfEvents),
+//           currentCity: location,
+//         });
+//       }
+//     });
+//   };
+
+//   //I this function will update tbe number of events of app.state fom <NumberOfEvents>
+//   updateNumberOfEvents(eventNumber) {
+//     this.setState({ numberOfEvents: eventNumber });
+//     const { currentCity } = this.state;
+//     this.updateEvents(currentCity, eventNumber);
+//   }
+
+//   async componentDidMount() {
+//     const { numberOfEvents } = this.state;
+//     this.mounted = true;
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const code = searchParams.get("code");
+//     this.setState({ showWelcomeScreen: !code });
+//     if (code && this.mounted) {
+//       getEvents().then((events) => {
+//         if (this.mounted) {
+//           this.setState({
+//             events: events.slice(0, numberOfEvents),
+//             locations: extractLocations(events),
+//           });
+//         }
+//       });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.mounted = false;
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <CitySearch
+//           locations={this.state.locations}
+//           updateEvents={this.updateEvents}
+//           numberOfEvents={this.state.numberOfEvents}
+//         />
+
+//         <NumberOfEvents
+//           updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)}
+//         />
+
+//         <EventList events={this.state.events} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
