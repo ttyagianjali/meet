2d9c3f778a7b381426bd5b41157ea230a78bf71d
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-18 01:46:40 -0400

    Revert "number of events working"
    This reverts commit 38e7ad7503faa83461b822239e096c210d7483bf.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,3 +1,49 @@
+// import React, { Component } from "react";
+
+// class NumberOfEvents extends Component {
+//   state = {
+//     numberOfEvents: 32,
+//     infoText: "",
+//   };
+
+//   handleInputChanged = (event) => {
+//     const value = event.target.value;
+//     if (isNaN(value)) {
+//       this.setState({
+//         infoText: "Please write a number",
+//         numberOfEvents: value,
+//       });
+//     } else if (value < 1 || value > 32) {
+//       this.setState({
+//         infoText: "Please select a number from 1 to 32",
+//         numberOfEvents: value,
+//       });
+//     } else {
+//       this.setState({
+//         numberOfEvents: value,
+//         infoText: "",
+//       });
+//       this.props.updateNumberOfEvents(event.target.value);
+//     }
+//   };
+
+//   render() {
+//     const numberOfEvents = this.state.numberOfEvents;
+//     return (
+//       <div className="numberOfEvents">
+//         <input
+//           type="text"
+//           className="EventsNumber"
+//           value={numberOfEvents}
+//           onChange={(e) => this.handleInputChanged(e)}
+//         />
+//         <label>Select Number of Events</label>
+//       </div>
+//     );
+//   }
+// }
+// export default NumberOfEvents;
+
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
@@ -27,7 +73,7 @@ class NumberOfEvents extends Component {
     }
   };
 
-  render() {
+    render() {
     const numberOfEvents = this.state.numberOfEvents;
     return (
       <div className="numberOfEvents">
@@ -42,4 +88,5 @@ class NumberOfEvents extends Component {
     );
   }
 }
+
 export default NumberOfEvents;
