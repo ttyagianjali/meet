02cff889770430cabf380babfdb4d041404b0f73
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-18 01:36:43 -0400

    displaying all suggestions in the suggestion box now with 100% coverage.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,53 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents } from "./api";
+import { extractLocations } from "./api";
+import "./nprogress.css";
 
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    currentCity: "all",
+  };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          numberOfEvents={this.state.numberOfEvents}
+        />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
