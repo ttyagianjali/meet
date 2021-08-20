a0e15bf73ff8b58d6d3427e425609abdd3f07cc2
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-20 00:46:19 -0400

    EndToEndTests Passed.

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,19 +1,38 @@
-// import puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
 
-// describe("show/hide an event details", () => {
-//   test("An event element is collapsed by default", async () => {
-//     beforeAll(async () => {
-//       jest.setTimeout(30000);
-//     });
-//     const browser = await puppeteer.launch();
+let browser;
+let page;
+beforeAll(async () => {
+  jest.setTimeout(300000);
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 150,
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+  await page.waitForSelector(".Event");
+});
 
-//     const page = await browser.newPage();
-//     await page.goto("http://localhost:3000/");
+afterAll(() => {
+  browser.close();
+});
 
-//     await page.waitForSelector(".event");
+describe("show/hide an event details", () => {
+  test("An event element is collapsed by default", async () => {
+    await expect(page.$(".Event .EventDetails")).resolves.toBe(null);
+    await expect(page.$(".showLess")).resolves.toBe(null);
+  });
+  test("User can expand an event to see its details and the Show less Button", async () => {
+    await page.click(".Event .showMore");
+    const eventDetails = await page.$(".EventDetails");
+    const showLessButton = await page.$(".showLess");
 
-//     const eventDetails = await page.$(".event .event__Details");
-//     expect(eventDetails).toBeNull();
-//     browser.close();
-//   });
-// });
+    expect(eventDetails).toBeDefined();
+    expect(showLessButton).toBeDefined();
+  });
+
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".Event .showLess");
+    await expect(page.$(".EventDetails")).resolves.toBe(null);
+  });
+});
